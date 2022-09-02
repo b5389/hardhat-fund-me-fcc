a114b749b39eef66eecab77412073a702e3564b3
abb <abb@LAPTOP-O2CAO9MS.localdomain>
2022-09-02 16:09:32 +0800

    initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,43 @@
+// async function deployFunc() {
+//     console.log("Hi!")
+// }
+// module.exports.default = deployFunc
+
+const { getNamedAccounts, deployments, network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+// hre: hardhat runtime environment
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+    // 本地
+    if (chainId == 31337) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    // 主链与测试链
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    log("----------------------------------------------------")
+    log("Deploying FundMe and waiting for confirmations...")
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress],
+        log: true,
+        // we need to wait if on a live network so we can verify properly
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    log(`FundMe deployed at ${fundMe.address}`)
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, [ethUsdPriceFeedAddress])
+    }
+}
+module.exports.tags = ["all", "fundme"]
